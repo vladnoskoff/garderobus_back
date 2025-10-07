956828846eaab69ec15980f2f44e247ccd759340
Vlad Noskov <inoskoff@me.com>
2025-10-07 19:20:32 +0000

    2

--- settings.py
+++ settings.py
@@ -9,8 +9,8 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent
 CLOTHES_IMAGE_DIR = BASE_DIR / os.getenv("CLOTHES_IMAGE_DIR", "clothes_images")
 MANNEQUIN_IMAGE_DIR = BASE_DIR / os.getenv("MANNEQUIN_IMAGE_DIR", "mannequins")
-CLOTHES_IMAGE_URL_PREFIX = os.getenv("CLOTHES_IMAGE_URL_PREFIX", "/clothes_images")
-MANNEQUIN_IMAGE_URL_PREFIX = os.getenv("MANNEQUIN_IMAGE_URL_PREFIX", "/mannequins")
+CLOTHES_IMAGE_URL_PREFIX = os.getenv("CLOTHES_IMAGE_URL_PREFIX", "http://aapanel-api.noksovsteam.ru/clothes_images")
+MANNEQUIN_IMAGE_URL_PREFIX = os.getenv("MANNEQUIN_IMAGE_URL_PREFIX", "http://aapanel-api.noksovsteam.ru/mannequins")
 TEST_PERSON_IMAGE_URL = os.getenv(
     "TEST_PERSON_IMAGE_URL",
     "http://aapanel-api.noksovsteam.ru/chkaf/clothes_images/static/test_mannequin.png",
