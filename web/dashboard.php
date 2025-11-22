f6d6f7d0c20c1ca7976ddeb5bb0e39fd83ee7544
Vlad Noskov <inoskoff@me.com>
2025-11-22 20:18:40 +0300

    Use HTTPS API base URL by default

--- web/dashboard.php
+++ web/dashboard.php
@@ -1,5 +1,5 @@
 <?php
-$apiBaseUrl = getenv('API_BASE_URL') ?: 'http://aapanel-api.noksovsteam.ru';
+$apiBaseUrl = getenv('API_BASE_URL') ?: 'https://aapanel-api.noksovsteam.ru';
 ?>
 <!DOCTYPE html>
 <html lang="ru">
